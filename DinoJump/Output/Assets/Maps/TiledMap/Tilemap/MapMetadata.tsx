2f623123053a78cf69c9dfd92ee169f345a0235d
TheimerTR <albertohidalgo268@gmail.com>
2022-11-30 13:13:25 +0100

    Metadata

---
+++ DinoJump/Output/Assets/Maps/TiledMap/Tilemap/MapMetadata.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="MapMetadata" tilewidth="18" tileheight="18" tilecount="3" columns="3">
+ <image source="../../MapMetadata.png" width="67" height="34"/>
+</tileset>
